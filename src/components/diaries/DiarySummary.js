05457396996569063fac373b750382fbea4d5282
developer-junaid <qjunaid623@gmail.com>
2021-01-21 19:36:59 +0500

    Updated DiarySummary Component,
    Next Diary Details

--- src/components/diaries/DiarySummary.js
+++ src/components/diaries/DiarySummary.js
@@ -4,7 +4,17 @@ const DiarySummary = () => {
   return (
     <div className="card z-depth-2 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <div className="card-title">Title</div>
+        <div className="card-title">
+          Title
+          <a href="#!" class="secondary-content">
+            <i class="material-icons">lock</i>
+          </a>
+        </div>
+
+        <span class="new badge" data-badge-caption="saved entries">
+          3
+        </span>
+
         <p>Posted by Author Name</p>
         <p className="grey-text">23 September, 2am</p>
       </div>
