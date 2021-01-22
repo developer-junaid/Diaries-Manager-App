79bb3e3c10348df0e7bd15a08f84b540f74dca07
developer-junaid <qjunaid623@gmail.com>
2021-01-22 16:41:53 +0500

    Configure Firebase in our app

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBGDD9xd89vlHyVcINP7eWVgzuou7yH0NE",
+  authDomain: "diaries-manager.firebaseapp.com",
+  projectId: "diaries-manager",
+  storageBucket: "diaries-manager.appspot.com",
+  messagingSenderId: "871095957098",
+  appId: "1:871095957098:web:0e97f82ea4279ea4d5fc13",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
