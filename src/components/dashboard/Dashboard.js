2b55d995f5ee54dada4c6701c6df150572bd5263
developer-junaid <qjunaid623@gmail.com>
2021-01-27 16:36:45 +0500

    Added Spinners for loading data from database
    NEXT: createEntry: add entry to database

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,19 +3,37 @@ import DiariesList from "./../diaries/DiariesList";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { BeatLoader } from "react-spinners";
 
 // Dashboard
 class Dashboard extends Component {
   render() {
     const { diaries } = this.props;
     // Return Dashboard
-    return (
-      <div className="dashboard container">
-        <div className="row">
-          <div className="col s12 m6">
-            <DiariesList diaries={diaries} />
+    if (diaries) {
+      return (
+        <div className="dashboard container">
+          <div className="row">
+            <div className="col s12 m6">
+              <DiariesList diaries={diaries} />
+            </div>
           </div>
         </div>
+      );
+    }
+
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          minHeight: "90vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <BeatLoader loading />
       </div>
     );
   }
--- src/components/entries/EntriesList.js
+++ src/components/entries/EntriesList.js
@@ -1,31 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import EntrySummary from "./EntrySummary";
 import { Link } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
 
 const EntriesList = ({ id, entries, diaryName }) => {
-  return (
-    <div className="project-list section">
-      <h4 className="card-title" style={{ color: "#424242" }}>
-        {diaryName}
-      </h4>
-      <div className="divider"></div>
+  // handle loading state
+  const [isLoading, setIsLoading] = useState(true);
 
-      {entries &&
-        entries.map((entry) => (
-          <EntrySummary diaryId={id} key={entry.id} entry={entry} />
-        ))}
+  useEffect(() => {
+    // If entries are there make loader false
+    if (entries.length) {
+      setIsLoading(false);
+    }
+  }, [entries]);
 
-      <div className="fixed-action-btn">
-        <Link
-          title="create new entry"
-          className="btn-floating btn-large  waves-effect hoverable waves-light green"
-          to={`/diary/${id}/create`}
-        >
-          <i className="material-icons">add</i>
-        </Link>
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          minHeight: "90vh",
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <BeatLoader loading />
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="project-list section">
+        <h4 className="card-title" style={{ color: "#424242" }}>
+          {diaryName}
+        </h4>
+        <div className="divider"></div>
+
+        {entries &&
+          entries.map((entry) => (
+            <EntrySummary diaryId={id} key={entry.id} entry={entry} />
+          ))}
+
+        <div className="fixed-action-btn">
+          <Link
+            title="create new entry"
+            className="btn-floating btn-large  waves-effect hoverable waves-light green"
+            to={`/diary/${id}/create`}
+          >
+            <i className="material-icons">add</i>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default EntriesList;
